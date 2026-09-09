df84fa7b29a718229cc98e10731f4fdf43bdf20c
learsi8918-hub <learsi8918@gmail.com>
2026-09-09 15:01:13 -0300

    Update saude_model.dart

--- lib/models/saude_model.dart
+++ lib/models/saude_model.dart
@@ -1,6 +1,10 @@
+/// -----------------------------------------------------------------------
+/// MODELO DE DADOS DA FICHA MÉDICA (SANIDADE DO PLANTEL)
+/// -----------------------------------------------------------------------
+
 class OcorrenciaSaude {
   final String id;
-  final String identificadorAve; // Conecta com o identificadorOficial (Clube-Anilha)
+  final String identificadorAve; // Conecta com o identificadorOficial da Ave (Clube-Anilha)
   final DateTime dataDiagnostico;
   final String doencaOuSintoma;
   final String tratamentoAplicado;
@@ -14,4 +18,32 @@ class OcorrenciaSaude {
     required this.tratamentoAplicado,
     required this.statusTratamento,
   });
+
+  /// -----------------------------------------------------------------------
+  /// MAPEAMENTO SERIALIZADOR PARA PERSISTÊNCIA NO SQLITE
+  /// -----------------------------------------------------------------------
+
+  // Converte o prontuário em um Mapa (JSON/Dicionário) para salvar no banco
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'identificadorAve': identificadorAve,
+      'dataDiagnostico': dataDiagnostico.toIso8601String(), // Datas viram texto ISO
+      'doencaOuSintoma': doencaOuSintoma,
+      'tratamentoAplicado': tratamentoAplicado,
+      'statusTratamento': statusTratamento,
+    };
+  }
+
+  // Reconstrói o prontuário a partir dos dados em formato de Mapa vindos do SQLite
+  factory OcorrenciaSaude.fromMap(Map<String, dynamic> map) {
+    return OcorrenciaSaude(
+      id: map['id'],
+      identificadorAve: map['identificadorAve'],
+      dataDiagnostico: DateTime.parse(map['dataDiagnostico']),
+      doencaOuSintoma: map['doencaOuSintoma'],
+      tratamentoAplicado: map['tratamentoAplicado'],
+      statusTratamento: map['statusTratamento'] ?? 'Em Tratamento',
+    );
+  }
 }
